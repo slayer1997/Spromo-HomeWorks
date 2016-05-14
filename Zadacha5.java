2bae8d5c3de75d6d9f4a2efca243fa8d60ff98ea
slayer1997 <vedmedenkobogdan@gmail.com>
2016-05-15 00:30:16 +0300

    Create Zadacha5.java

---
+++ Zadacha5.java
@@ -0,0 +1,8 @@
+public class Zadacha5 {
+    public static void main(String[] args){
+        int radius = 15;
+        double length = Math.PI * 2 * radius;
+        System.out.println(length);
+
+    }
+}
